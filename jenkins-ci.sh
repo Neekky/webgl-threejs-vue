1966eb2ee46bf6e04edf152d4f3ac9101ca364c0
CengChao <chao1.zeng@aihuishou.com>
2023-01-13 13:57:15 +0800

    feat: 修改jenkins文件

--- jenkins-ci.sh
+++ jenkins-ci.sh
@@ -1,5 +1,13 @@
-node -v
-npm -v
-# npm install yarn -g
+#!/bin/sh
+
+node --version
+
+echo "---------设置使用源--------"
 yarn config set registry https://registry.npm.taobao.org
+
+echo "---------查看当前源--------"
+yarn config get registry
+
+npm -v
+
 yarn
