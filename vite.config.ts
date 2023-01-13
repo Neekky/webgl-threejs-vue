15ec0def02cab236c2fe10499edf9a8cff71bf65
CengChao <chao1.zeng@aihuishou.com>
2023-01-13 23:44:50 +0800

    feat: 更新vite配置

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "threejs",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
